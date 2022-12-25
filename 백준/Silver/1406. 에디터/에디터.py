cbe93bdf275b03bc689cb9fd2726ea5b75f90996
jhjalison01 <79524041+jhjalison01@users.noreply.github.com>
2022-12-25 20:28:48 +0900

    [Silver II] Title: 에디터, Time: 312 ms, Memory: 36800 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -0,0 +1,22 @@
+import sys
+
+st1=list(sys.stdin.readline().rstrip())
+n=int(sys.stdin.readline())
+st2=[]
+
+for _ in range(n):
+    command=list(sys.stdin.readline().split())
+    if command[0]=='L':
+        if st1:
+            st2.append(st1.pop())
+    elif command[0]=='D':
+        if st2:
+            st1.append(st2.pop())
+    elif command[0]=='B':
+        if st1:
+            st1.pop()
+    elif command[0]=='P':
+        st1.append(command[1])
+
+st1.extend(reversed(st2))
+print("".join(st1))
