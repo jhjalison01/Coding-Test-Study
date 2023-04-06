589868f2630974e8f92c67173a7d280a69a859ec
jhjalison01 <79524041+jhjalison01@users.noreply.github.com>
2023-04-06 23:11:39 +0900

    [Silver II] Title: DFS와 BFS, Time: 456 ms, Memory: 34184 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,29 +1,10 @@
 from collections import deque
 
-n, m,v = map(int, input().split())
-
-#그래프를 리스트 자료형으로 표현
-graph=[]
-for i in range(1001):
-  graph.append([])
-
-#각 노드가 연결된 정보를 표현
-for i in range(m):
-  a, b =map(int,input().split())
-  graph[a].append(b)
-  graph[b].append(a)
-
-#각 노드가 방문된 정보를 리스트 자료형으로 표현
-visited=[False]*1001
-visited_bfs=[False]*1001
-
 #dfs 메소드 정의
 def dfs(graph, v, visited):
   #현재 노드를 방문 처리
   visited[v]=True
   print(v, end=' ')
-  #번호가 작은 노드부터 먼저 방문하기 위해 숫자 정렬
-  graph[v].sort()
   #현재 노드와 연결된 노드 중 방문한 적이 없는 노드를 재귀적으로 방문
   for i in graph[v]:
     if not visited[i]:
@@ -40,14 +21,33 @@ def bfs(graph, v, visited):
     #큐에서 하나의 원소를 뽑아 출력한다
     v=queue.popleft()
     print(v, end=' ')
-    #번호가 작은 노드부터 먼저 방문하기 위해 숫자 정렬
-    graph[v].sort()
     #현재 노드에 연결된 노드 중 방문한 적이 없는 노드를 큐에 삽입&방문 처리
     for i in graph[v]:
       if not visited[i]:
         queue.append(i)
         visited[i]=True
-  
+
+n, m, v = map(int, input().split())
+
+#그래프를 리스트 자료형으로 표현
+graph=[]
+for i in range(n+1):
+  graph.append([])
+
+#각 노드가 연결된 정보를 표현
+for i in range(m):
+  a, b =map(int,input().split())
+  graph[a].append(b)
+  graph[b].append(a)
+
+#번호가 작은 노드부터 먼저 방문하기 위해 숫자 정렬
+for i in graph:
+  i.sort()
+
+#각 노드가 방문된 정보를 리스트 자료형으로 표현
+visited=[False]*(n+1)
+visited_bfs=[False]*(n+1)
+
 
 dfs(graph, v, visited)
 print()
