255e2115f3f3407d83d360798ed5111da04fd251
jhjalison01 <79524041+jhjalison01@users.noreply.github.com>
2024-07-06 01:07:15 +0900

    [Silver I] Title: 단지번호붙이기, Time: 64 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,49 +1,39 @@
+import sys
+input=sys.stdin.readline
 from collections import deque
 
-#지도의 크기 입력 받기
-n=int(input())
-#지도 정보 입력 받기
-graph=[[] for _ in range(n)]
+n = int(input())
+graph=[]
 for i in range(n):
-  data=input()
-  for j in range(n):
-    graph[i].append(int(data[j]))
+  a=list(input().rstrip())
+  graph.append(a)
 
-#상하좌우
 dx=[-1,1,0,0]
-dy=[0,0,1,-1]
+dy=[0,0,-1,1]
 
-#BFS 구현
 def bfs(x,y):
+  graph[x][y] = '2'
   q=deque()
   q.append((x,y))
-  result=1 #단지내 집의 수
-  graph[x][y]=2
-  #큐가 빌 때까지 반복
+  cnt=1
   while q:
     x,y=q.popleft()
-    for i in range(4): #현재 위치에서 상하좌우 확인
+    for i in range(4):
       nx=x+dx[i]
       ny=y+dy[i]
-      if 0<=nx<n and 0<=ny<n and graph[nx][ny]==1:
-        result+=1
+      if 0<=nx<n and 0<=ny<n and graph[nx][ny] == '1':
+        graph[nx][ny]='2'
+        cnt+=1
         q.append((nx,ny))
-        graph[nx][ny]=2 #탐색한 곳은 2로 표시
-  return result
+  return cnt
 
-answer=0 #총 단지수
-houses=[]#각 단지내 집의 수
-
+answer=[]
 for i in range(n):
   for j in range(n):
-    if graph[i][j]==1: #값이 1인 곳 탐색
-      house=bfs(i,j) #집의 수
-      answer+=1
-      houses.append(house)
+    if graph[i][j] == '1':
+      answer.append(bfs(i,j))
 
-#총 단지수 출력
-print(answer)
-#각 단지내 집의 수를 오름차순으로 정렬, 출력
-houses.sort()
-for x in houses:
-  print(x)
+print(len(answer))
+answer.sort()
+for num in answer:
+  print(num)
